f79e21b14a09d81df6b105b0fa853fa2696a9abe
Sayyed-rehan <rehansayyed1216@gmail.com>
2023-11-20 22:21:24 +0530

    login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Login/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
